ed6fc3367691f02f106032afbbc229fde0108de9
RaviAnand59 <119591464+RaviAnand59@users.noreply.github.com>
2023-06-28 18:48:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,35 +2,46 @@
 # Posted on Github on 9 JAN 2023
 
 # libraries needed
+import keyboard
 import mediapipe as mp
 import cv2
 from math import sqrt
 import win32api
 import pyautogui
 
+
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 
-rclick = 0  #right click
-click = 0   #left click
-dclick = 0  #double click
-scroll = 0  #scroll down
-
+rclick = 0  # right click
+click = 0  # left click
+dclick = 0  # double click
+scroll = 0  # scroll down
+# Declare a flag variable for the volume control mode
+volume_control_mode = False
+volume_down = 0
 
 video = cv2.VideoCapture(0)
 
-
 with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.8) as hands:
-    #to manage smoothening according to camera quality,background disturbance
+    _, picture = video.read()
+    image = cv2.cvtColor(picture, cv2.COLOR_BGR2RGB)
+    image = cv2.flip(image, 1)
+    imageHeight, imageWidth, _ = image.shape
+    cv2.namedWindow('Hand Tracking', cv2.WINDOW_NORMAL)
+    cv2.setWindowProperty('Hand Tracking', cv2.WND_PROP_TOPMOST, 1)
+    # to manage smoothening according to camera quality,background disturbance
     while video.isOpened():
+        if keyboard.is_pressed('a'):
+            volume_control_mode = not volume_control_mode  # toggle the variable's value
         _, picture = video.read()
         image = cv2.cvtColor(picture, cv2.COLOR_BGR2RGB)
 
-        #converting to RGB format for mediapipe
+        # converting to RGB format for mediapipe
 
         image = cv2.flip(image, 1)
 
-        imageHeight, imageWidth, _ = image.shape
+        # imageHeight, imageWidth, _ = image.shape
 
         results = hands.process(image)
 
@@ -39,7 +50,7 @@ with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.8) a
         # if there is a hand present in frame
         if results.multi_hand_landmarks:
             for num, hand in enumerate(results.multi_hand_landmarks):
-                #iterating over all marked points in hands
+                # iterating over all marked points in hands
                 mp_drawing.draw_landmarks(image, hand, mp_hands.HAND_CONNECTIONS,
                                           mp_drawing.DrawingSpec(color=(350, 90, 190), circle_radius=3),
                                           )
@@ -55,7 +66,7 @@ with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.8) a
 
                     point = str(point)
 
-                    #Storing x and y coordinates of finger landmarks in use
+                    # Storing x and y coordinates of finger landmarks in use
 
                     if point == 'HandLandmark.THUMB_TIP':
                         try:
@@ -72,7 +83,7 @@ with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.8) a
                             indexfingertip_x = pixelCoordinatesLandmark[0]
                             indexfingertip_y = pixelCoordinatesLandmark[1]
                             win32api.SetCursorPos((indexfingertip_x * 4, indexfingertip_y * 5))
-                            #multiplied with 4 and 5 to adjust ratio between camera size and screen size
+                            # multiplied with 4 and 5 to adjust ratio between camera size and screen size
 
                         except:
                             pass
@@ -127,29 +138,29 @@ with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.8) a
                         except:
                             pass
 
-                   #start of main operation
+                    # start of main operation
 
-
-                   #left click
+                if volume_control_mode == False:
+                    # left click
                     try:
 
-                        if middlefingertip_y < middlefingerpip_y  and thumbfingertip_x > thumbip_x :
+                        if middlefingertip_y < middlefingerpip_y and thumbfingertip_x > thumbip_x:
                             click = click + 1
-                            if click % 30 == 0:
-                                #this is done to avoid multiple clicks due to continously running while loop
+                            if click % 4 == 0:
+                                # this is done to avoid multiple clicks due to continously running while loop
                                 print("single click")
                                 pyautogui.click()
                     except:
                         pass
 
-                    #right click
+                    # right click
                     try:
                         Distance2 = sqrt(
                             (pinkyfingertip_x - thumbfingertip_x) ** 2 + (pinkyfingertip_y - thumbfingertip_y) ** 2)
 
                         if Distance2 < 20:
                             rclick = rclick + 1
-                            if rclick % 20 == 0:
+                            if rclick % 2 == 0:
                                 print("right click")
                                 pyautogui.rightClick()
 
@@ -160,7 +171,7 @@ with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.8) a
                     try:
                         if middlefingertip_y > middlefingerpip_y and thumbfingertip_x > thumbip_x:
                             dclick = dclick + 1
-                            if dclick % 80 == 0:
+                            if dclick % 5 == 0:
                                 print("double click")
                                 pyautogui.doubleClick()
 
@@ -168,33 +179,44 @@ with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.8) a
                     except:
                         pass
 
-                    #scroll down
+                    # scroll down
                     try:
                         Distance = sqrt(
                             (indexfingertip_x - thumbfingertip_x) ** 2 + (indexfingertip_y - thumbfingertip_y) ** 2)
 
                         if Distance < 20:
-                            pyautogui.scroll(-100)
+                            pyautogui.scroll(-250)
 
 
 
                     except:
                         pass
 
-                    #scroll up
+                    # scroll up
                     try:
                         Distance3 = sqrt(
                             (middlefingertip_x - thumbfingertip_x) ** 2 + (middlefingertip_y - thumbfingertip_y) ** 2)
 
                         if Distance3 < 20:
-                            pyautogui.scroll(100)
-
-
-
+                            pyautogui.scroll(250)
                     except:
                         pass
 
+                if volume_control_mode:
+                    # Increase volume if the thumb is above the index finger
+                    try:
+                            if thumbfingertip_y > indexfingertip_y:
+                                # Add code to increase the volume here
+                                pyautogui.press('volumeup')
 
+                            # Decrease volume if the thumb is below the index finger
+                            elif thumbfingertip_y < indexfingertip_y:
+                                # Add code to decrease the volume here
+                                volume_down = volume_down + 1
+                                if volume_down % 3 == 0:
+                                   pyautogui.press('volumedown')
+                    except:
+                        pass
 
 
 
@@ -202,8 +224,10 @@ with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.8) a
 
         cv2.imshow('Hand Tracking', image)
 
-        #wait for 5 milliseconds for quitting
-        #press e to exit
+
+
+        # wait for 5 milliseconds for quitting
+        # press e to exit
         if cv2.waitKey(5) & 0xFF == ord('e'):
             break
 
